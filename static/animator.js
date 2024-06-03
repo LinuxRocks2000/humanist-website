c7af2031ff3eaca242d150be5415013a2b0dfd4a
Lazy Hippopotamus <lazy@hippo.com>
2024-06-03 15:10:05 -0400

    the effect looks cool

--- static/animator.js
+++ static/animator.js
@@ -1,10 +1,28 @@
 // CSS property animator
 
+const timing = { // timing functions
+    // they take a percentage of animation time completion and output a percentage of animation completion
+    linear(time) {
+        return time;
+    },
+    quadSmooth(time) { // quadratic smooth like the CSS animation type
+        if (time <= 0.5) {
+            return 2 * time * time;
+        }
+        else {
+            return -2 * (time - 1) ** 2 + 1
+        }
+    }
+};
+
+
 class CSSAnimator {
-    constructor(target, property, aTime, units = "px", current = 0) {
+    constructor(target, property, aTime, units = "px", current = 0, timingFunction = timing.quadSmooth) {
+        this.timingFunction = timingFunction;
         this.target = target;
         this.property = property;
-        this.animationDuration = aTime; // in milliseconds
+        this.animationDuration = 0; // in milliseconds
+        this.defaultAnimationDuration = aTime; // animationDuration is managed by set()
         this.units = units;
         this.current = current;
         this.start = this.current;
@@ -17,8 +35,16 @@ class CSSAnimator {
         if (this.goalTime > cTime) {
             var timeTill = this.goalTime - cTime;
             var perc = 1 - timeTill / this.animationDuration; // the percentage of completion in the current animation step
-            this.current = this.start + (this.goal - this.start) * perc;
+            this.current = this.start + (this.goal - this.start) * this.timingFunction(perc);
             this.target.style.setProperty(this.property, this.current + this.units);
+        }
+        else {
+            this.target.style.setProperty(this.property, this.goal + this.units);
+            if (this.oncomplete) {
+                var oldOncomplete = this.oncomplete;
+                this.oncomplete = undefined;
+                oldOncomplete(); // the oncomplete function needs to be able to meaningfully set oncomplete, so we pop out the function pointer before clearing oncomplete().
+            }
         }
     }
 
@@ -30,10 +56,16 @@ class CSSAnimator {
         animFrame();
     }
 
-    set(goal) {
+    set(goal, oncomplete = undefined, dur = this.defaultAnimationDuration) {
         this.start = this.current;
         this.goal = goal;
+        this.animationDuration = dur;
         this.goalTime = window.performance.now() + this.animationDuration;
+        this.oncomplete = oncomplete;
+    }
+
+    add(amount) {
+        this.set(this.getC() + amount);
     }
 
     getC() { // return the current INTERPOLATED step (the current value of the css property)
--- static/slideshow.js
+++ static/slideshow.js
@@ -1,36 +1,93 @@
 const slides = Array.from(document.getElementById("slideshow-container").children);
 const positiondicator_el = document.getElementById("positiondicator");
 const pde_w = positiondicator_el.getBoundingClientRect().width;
-const positiondicator = new CSSAnimator(positiondicator_el, "--fill-point", 250);
+const positiondicator = new CSSAnimator(positiondicator_el, "--fill-point", 250, "px", 0, timing.linear);
+const containerLeft = new CSSAnimator(document.getElementById("slideshow-container"), "--offset-left", 500);
 
 var slideshow_cur = 0;
 
+function coterminal(n, about) {
+    while (n < 0) {
+        n += about;
+    }
+    while (n >= about) {
+        n -= about;
+    }
+    return n;
+}
+
+function absdist(one, two) {
+    return Math.abs(one - two);
+}
+
 function setSlideshow(num) { // set the slideshow to an index
+    if (num < 0 || num >= slides.length) {
+        return;
+    } 
     slideshow_cur = num;
     Array.from(document.getElementsByClassName("slideshow-visible")).forEach(el => {
         el.classList.remove("slideshow-visible");
     });
     slides[num].classList.add("slideshow-visible");
-    positiondicator.set(num / slides.length * pde_w);
+    positiondicator.set((num) / slides.length * pde_w, () => {
+        if (num < slides.length) {
+            slidebump();
+        }
+    });
+    //containerLeft.add(-1 * slides[num].getBoundingClientRect().left);
 }
 
 var last = window.performance.now();
+var slideshowInterrupted = false;
 
-function main() { // bump up the slider
-    var cTime = window.performance.now();
-    var sElapsed = (cTime - last) / 1000;
-    last = cTime;
-    var slider_num = Math.floor(slides.length * positiondicator.getC() / pde_w); // the block the slider's in
-    if (slider_num < slides.length) {
-        positiondicator.set(positiondicator.getC() + sElapsed * 500);
-        if (slider_num != slideshow_cur) {
-            setSlideshow(slider_num);
-        }
-    }
-    requestAnimationFrame(main);
-}
-
+containerLeft.startAnimation();
 positiondicator.startAnimation();
 setSlideshow(0);
 
-main();
+function slidebump() { // bumps up the slider by one at a low speed. can be interrupted safely by pointer events.
+    var go_at = slideshow_cur + 1;
+    positiondicator.set((go_at) / slides.length * pde_w, () => {
+        setSlideshow(go_at);
+    }, 2000);
+}
+
+var isDown = false;
+var dragStartPoint = {
+    x: 0,
+    y: 0
+};
+
+window.addEventListener("pointerdown", evt => {
+    isDown = true;
+    dragStartPoint.x = evt.clientX;
+    dragStartPoint.y = evt.clientY;
+});
+
+window.addEventListener("touchstart", evt => {
+    isDown = true;
+    dragStartPoint.x = evt.touches[0].clientX;
+    dragStartPoint.y = evt.touches[0].clientY;
+});
+
+function mouseup(deltaX, sensitivity = 100) {
+    if (Math.abs(deltaX) > sensitivity) {
+        if (deltaX > 0) {
+            setSlideshow(slideshow_cur - 1);
+        }
+        else {
+            setSlideshow(slideshow_cur + 1);
+        }
+        slideshowInterrupted = true;
+    }
+    isDown = false;
+}
+
+window.addEventListener("pointerup", evt => {
+    var deltaX = evt.clientX - dragStartPoint.x;
+    mouseup(deltaX);
+});
+
+window.addEventListener("touchend", evt => {
+    var deltaX = evt.changedTouches[0].clientX - dragStartPoint.x;
+    mouseup(deltaX, 30);
+});
